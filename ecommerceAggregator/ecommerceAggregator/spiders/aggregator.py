12eee9f558ee7fca64b926e214f7712bc6b8cdac
Nazmus Shakib <44675374+shakib500@users.noreply.github.com>
2019-03-08 16:25:06 +0600

    working on next page url

--- ecommerceAggregator/ecommerceAggregator/spiders/aggregator.py
+++ ecommerceAggregator/ecommerceAggregator/spiders/aggregator.py
@@ -4,17 +4,18 @@ import scrapy
 
 class AggregatorSpider(scrapy.Spider):
     name = 'aggregator'
-    allowed_domains = ['www.startech.com.bd/component/processor']
-    start_urls = ['https://www.startech.com.bd/component/processor']
+    allowed_domains = ['startech.com.bd/component/processor']
+    start_urls = ['https://startech.com.bd/component/processor']
 
     def parse(self, response):
         processor_details = response.xpath('//*[@class="col-xs-12 col-md-4 product-layout grid"]')
         for processor in processor_details:
             name = processor.xpath('.//h4/a/text()').extract_first()
             price = processor.xpath('.//*[@class="price space-between"]/span/text()').extract_first()
-
             print ('\n')
             print (name)
             print (price)
             print ('\n')
-        next_page = response.xpath('//*[@class="pagination"]/li/a').extract()
+        next_page_url = response.xpath('//*[@class="pagination"]/li/a/@href').extract_first()
+        # absolute_next_page_url = response.urljoin(next_page_url)
+        yield scrapy.Request(next_page_url)
