74cdb8759f390a83bf2fe813d65bfc7f2ce8934e
Nazmus Shakib <44675374+shakib500@users.noreply.github.com>
2019-03-08 17:58:30 +0600

    next page url problem solved

--- ecommerceAggregator/ecommerceAggregator/spiders/aggregator.py
+++ ecommerceAggregator/ecommerceAggregator/spiders/aggregator.py
@@ -4,7 +4,7 @@ import scrapy
 
 class AggregatorSpider(scrapy.Spider):
     name = 'aggregator'
-    allowed_domains = ['startech.com.bd/component/processor']
+    allowed_domains = ['www.startech.com.bd']
     start_urls = ['https://startech.com.bd/component/processor']
 
     def parse(self, response):
@@ -16,6 +16,13 @@ class AggregatorSpider(scrapy.Spider):
             print (name)
             print (price)
             print ('\n')
-        next_page_url = response.xpath('//*[@class="pagination"]/li/a/@href').extract_first()
         # absolute_next_page_url = response.urljoin(next_page_url)
-        yield scrapy.Request(next_page_url)
+        next_page_url = response.css('.pagination li:last-child a::attr(href)').extract_first()
+        if next_page_url:
+            yield scrapy.Request(next_page_url)
+        # yield scrapy.Request(next_page_url, callback=self.parse_next_page)
+
+    # def parse_next_page(self, response):
+    #     # next_page_url = response.xpath('//*[@class="pagination"]/li/a/@href').extract_first()
+    #     self.logger.info("Visited %s", response.url)
+    #     # yield scrapy.Request(next_page_url)
