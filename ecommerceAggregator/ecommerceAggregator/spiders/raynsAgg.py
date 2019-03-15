b223a2b047f5bb0ec918de0667d82cfa73e94463
Nazmus Shakib <44675374+shakib500@users.noreply.github.com>
2019-03-16 00:12:30 +0600

    scraped rayns processor details

---
+++ ecommerceAggregator/ecommerceAggregator/spiders/raynsAgg.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class RaynsaggSpider(scrapy.Spider):
+    name = 'raynsAgg'
+    allowed_domains = ['ryanscomputers.com']
+    start_urls = ['https://ryanscomputers.com/components/processor.html']
+
+    def parse(self, response):
+        processor_details = response.xpath('//*[@class="item col-lg-4 col-md-4 col-sm-6"]')
+
+        for processor in processor_details:
+            name = processor.xpath('.//h2/a/text()').extract_first()
+            price = processor.xpath('.//*[@class="price"]/text()').extract_first()
+            # print ('\n')
+            # print (name)
+            # print (price)
+            # print ('\n')
+            yield{'Name': name,
+                  'Price': price}
+
+        next_page_url = response.xpath('//*[@class="next i-next"]/@href').extract_first()
+        yield scrapy.Request(next_page_url)
