46de8db418e0c78a59849d51d7c85ffa75d34665
Nazmus Shakib <44675374+shakib500@users.noreply.github.com>
2019-03-17 20:52:52 +0600

    processor,motherboard,gpu added

--- ecommerceAggregator/ecommerceAggregator/spiders/raynsAgg.py
+++ ecommerceAggregator/ecommerceAggregator/spiders/raynsAgg.py
@@ -5,7 +5,7 @@ from scrapy.http import Request
 class RaynsaggSpider(scrapy.Spider):
     name = 'raynsAgg'
     allowed_domains = ['ryanscomputers.com']
-    start_urls = ['https://ryanscomputers.com/components/processor.html']
+    start_urls = ['https://ryanscomputers.com/components/processor.html','https://ryanscomputers.com/components/mainboard.html','https://ryanscomputers.com/components/graphics-card.html']
 
     def parse(self, response):
         processor_details = response.xpath('//*[@class="item col-lg-4 col-md-4 col-sm-6"]')
@@ -25,4 +25,6 @@ class RaynsaggSpider(scrapy.Spider):
                   'Img': img }
 
         next_page_url = response.xpath('//*[@class="next i-next"]/@href').extract_first()
-        yield scrapy.Request(next_page_url)
+        # yield scrapy.Request(next_page_url)
+        if next_page_url:
+            yield scrapy.Request(next_page_url)
