730fd86c7c9ffdc35d4bf1660b88abfe491e37b0
SafinMahmud <safin5216@gmail.com>
2019-03-18 22:10:35 +0600

    adding data from csv

--- aggregator/product/models.py
+++ aggregator/product/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Product(models.Model):
     name = models.CharField(max_length=200)
-    price = models.IntegerField(default=0)
+    price = models.CharField(max_length=200)
     url = models.CharField(max_length=200)
     img = models.CharField(max_length=200)
 
