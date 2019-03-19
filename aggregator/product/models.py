16f0982c8c3abbde3f17bc67eeb45307fb321055
Nazmus Shakib <44675374+shakib500@users.noreply.github.com>
2019-03-19 20:02:07 +0600

    converting the string to integer

--- aggregator/product/models.py
+++ aggregator/product/models.py
@@ -7,4 +7,5 @@ class Product(models.Model):
     url = models.CharField(max_length=200)
     img = models.CharField(max_length=200)
 
-
+    # For converting the string into integer
+    # find_number = [n for n in re.findall(r'(\d+)', string.replace(',', ''))]
