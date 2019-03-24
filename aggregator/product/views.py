c0e8aae258c5759b0b3b895819a8e7da657d4d0d
SafinMahmud <safin5216@gmail.com>
2019-03-25 01:24:20 +0600

    sorting price

--- aggregator/product/views.py
+++ aggregator/product/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, HttpResponse
 
 from .models import Product 
 from django.db.models import Q
+import re
 
 def index(request):
     query = request.GET.get('q')
@@ -10,5 +11,13 @@ def index(request):
     	return render(request, "product/list.html",{'object_list':products})
     else:
     	results = Product.objects.filter(Q(name__icontains=query))
+    	price_list = Product.objects.values_list('price', flat=True).filter(Q(name__icontains=query))
+    	find_num = []
+    	for prod_price in price_list:
+    		find_num_temp = [n for n in re.findall(r'(\d+)',prod_price.replace(',',''))]
+    		y = ''.join(find_num_temp)
+    		find_num.append(int(y))
+    	find_num.sort()
+    	print(find_num)
     	return render(request, "product/list.html",{'object_list':results})
 
